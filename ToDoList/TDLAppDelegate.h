8739ac6c47729dc699f42326d98737ae22754503
Katherine Albitz <kalbitz@san.rr.com>
2014-06-03 18:00:57 -0700

    new files

---
+++ ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Paul Albitz on 6/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Paul Albitz on 6/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDLAppDelegate class]));
+    }
+}
